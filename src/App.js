4a87a070d144cda60777c12ce2cf3861d80bd866
Spinglez <42229372+Spinglez@users.noreply.github.com>
2019-04-17 18:43:27 -0700

    first commit on the redesign for my profile

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to my new Profile! It's under construction, stop back by later!</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/index.js
@@ -0,0 +1,7 @@
+// Allows exporting from a central Profile
+
+export {default as NotFound} from './404/notFound'
+export {default as Content} from './content/Content'
+export {default as Footer} from './footer/Footer'
+export {default as Header} from './header/Header'
+export {default as Welcome} from './welcome/Welcome'
